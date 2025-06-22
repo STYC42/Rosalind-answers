2f4f62ec2a379402940259e3efbd5f72074581aa
STYCA <styca42@proton.me>
2025-06-22 11:29:51 +0200

    DNA completed

---
+++ template.ml
@@ -0,0 +1,20 @@
+let read_file filename =
+  let ic = open_in filename in
+  let n = in_channel_length ic in
+  let s = really_input_string ic n in
+  close_in ic;
+  s
+
+
+let main s =
+  (* ... *)
+  ()
+
+
+let () =
+  if Array.length Sys.argv <> 2 then begin
+    prerr_endline "Usage: ./run.out <input_file>";
+    exit 1
+  end;
+  let input = read_file Sys.argv.(1) in
+  main input
