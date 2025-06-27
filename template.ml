04ea80c3d78c9790756d351d7fab2ee31dfaceaa
STYCA <styca42@proton.me>
2025-06-27 06:36:27 +0200

    Adding fasta reader to template

--- template.ml
+++ template.ml
@@ -5,6 +5,32 @@ let read_file filename =
   close_in ic;
   s
 
+let read_fasta filename =
+  let ic = open_in filename in
+  let rec read_lines acc current_id current_seq =
+    try
+      let line = input_line ic in
+      if String.length line > 0 && line.[0] = '>' then
+        let id = String.sub line 1 (String.length line - 1) in
+        let acc =
+          match current_id with
+          | Some id -> (id, String.concat "" (List.rev current_seq)) :: acc
+          | None -> acc
+        in
+        read_lines acc (Some id) []
+      else
+        read_lines acc current_id (line :: current_seq)
+    with End_of_file ->
+      let acc =
+        match current_id with
+        | Some id -> (id, String.concat "" (List.rev current_seq)) :: acc
+        | None -> acc
+      in
+      close_in ic;
+      List.rev acc
+  in
+  read_lines [] None []
+
 
 let main s =
   (* ... *)
